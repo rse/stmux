ddc9d6083ab343a49a5f7d2f476610b6e816eade
Ralf S. Engelschall <rse@engelschall.com>
2018-05-05 13:26:43 +0200

    bump year in all copyright messages

--- README.md
+++ README.md
@@ -299,7 +299,7 @@ Specification Examples
 License
 -------
 
-Copyright (c) 2017 Ralf S. Engelschall (http://engelschall.com/)
+Copyright (c) 2017-2018 Ralf S. Engelschall (http://engelschall.com/)
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
--- src/stmux.js
+++ src/stmux.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /*!
 **  stmux -- Simple Terminal Multiplexing for Node Environments
-**  Copyright (c) 2017 Ralf S. Engelschall <rse@engelschall.com>
+**  Copyright (c) 2017-2018 Ralf S. Engelschall <rse@engelschall.com>
 **
 **  Permission is hereby granted, free of charge, to any person obtaining
 **  a copy of this software and associated documentation files (the
