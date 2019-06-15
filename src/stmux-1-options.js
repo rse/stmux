f454fd40ec8e749500d8645a05c1734fe225465c
Ralf S. Engelschall <rse@engelschall.com>
2019-06-15 20:00:43 +0200

    bump year in all copyright messages

--- src/stmux-1-options.js
+++ src/stmux-1-options.js
@@ -1,6 +1,6 @@
 /*
 **  stmux -- Simple Terminal Multiplexing for Node Environments
-**  Copyright (c) 2017-2018 Ralf S. Engelschall <rse@engelschall.com>
+**  Copyright (c) 2017-2019 Dr. Ralf S. Engelschall <rse@engelschall.com>
 **
 **  Permission is hereby granted, free of charge, to any person obtaining
 **  a copy of this software and associated documentation files (the
