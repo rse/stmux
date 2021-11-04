c5598618ea465efce701bb158819ff6c33fa749d
Ralf S. Engelschall <rse@engelschall.com>
2021-11-04 09:06:19 +0100

    fix Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
         eslint: {
             options: {
-                configFile: "etc/eslint.yaml"
+                overrideConfigFile: "etc/eslint.yaml"
             },
             "stmux": [ "src/**/*.js" ]
         },
