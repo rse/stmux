8f7a54afccdc1bff7de795b328e8788e604c608a
Ralf S. Engelschall <rse@engelschall.com>
2026-07-19 10:03:25 +0200

    generate output with template literals

--- src/stmux-1-options.ts
+++ src/stmux-1-options.ts
@@ -65,10 +65,10 @@ export default <T extends Constructor<STMUXBase>>(Base: T) =>
 
             /*  short-circuit processing of "-v" command-line option  */
             if (this.argv.version) {
-                process.stderr.write(this.my.name + " " + this.my.version + " <" + this.my.homepage + ">\n")
-                process.stderr.write(this.my.description + "\n")
-                process.stderr.write("Copyright (c) 2017-2024 " + this.my.author.name + " <" + this.my.author.url + ">\n")
-                process.stderr.write("Licensed under " + this.my.license + " <http://spdx.org/licenses/" + this.my.license + ".html>\n")
+                process.stderr.write(`${this.my.name} ${this.my.version} <${this.my.homepage}>\n`)
+                process.stderr.write(`${this.my.description}\n`)
+                process.stderr.write(`Copyright (c) 2017-2026 ${this.my.author.name} <${this.my.author.url}>\n`)
+                process.stderr.write(`Licensed under ${this.my.license} <https://spdx.org/licenses/${this.my.license}.html>\n`)
                 process.exit(0)
             }
 
